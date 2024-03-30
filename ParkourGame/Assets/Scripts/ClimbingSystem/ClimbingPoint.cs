97c266f4e4f9d4ea66cff8450aebcb0ca9766c42
vlaud <whehdgus1207@naver.com>
2024-03-30 22:25:21 +0900

    Create Climbing Web

---
+++ ParkourGame/Assets/Scripts/ClimbingSystem/ClimbingPoint.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ClimbingPoint : MonoBehaviour
+{
+    public List<Neighbour> neighbours;
+
+    private void Awake()
+    {
+        var twoWayClimbNeighbour = neighbours.Where(n => n.isPointTwoWay);
+        foreach (var neighbour in twoWayClimbNeighbour)
+        {
+            neighbour.climbingPoint?.CreatePointConnection(this, -neighbour.pointDirection, neighbour.connetionType, neighbour.isPointTwoWay);
+        }
+    }
+
+    public void CreatePointConnection(ClimbingPoint climbingPoint, Vector2 pointDirection,
+        ConnetionType connetionType, bool isPointTwoWay)
+    {
+        var neighbour = new Neighbour()
+        {
+            climbingPoint = climbingPoint,
+            pointDirection = pointDirection,
+            connetionType = connetionType,
+            isPointTwoWay = isPointTwoWay
+        };
+
+        neighbours.Add(neighbour);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
+        foreach (var neighbour in neighbours)
+        {
+            if(neighbour.climbingPoint != null)
+                Debug.DrawLine(transform.position, neighbour.climbingPoint.transform.position, (neighbour.isPointTwoWay) ? Color.green : Color.black);
+        }
+    }
+}
+
+[System.Serializable]
+public class Neighbour
+{
+    public ClimbingPoint climbingPoint;
+    public Vector2 pointDirection;
+    public ConnetionType connetionType;
+    public bool isPointTwoWay = true;
+}
+
+public enum ConnetionType { Jump, Move }
